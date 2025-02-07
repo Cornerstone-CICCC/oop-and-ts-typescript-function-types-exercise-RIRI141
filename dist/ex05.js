115033a083448928463995b085c927177f05d268
Ryoga <isimaruru.1712@gmail.com>
2025-02-06 18:37:10 -0800

    Doooone

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,13 +1,10 @@
+"use strict";
 // Create a function called sumAllNumbers.
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
-
-function sumAllNumbers(...result: number[]): number {
+function sumAllNumbers(...result) {
     let sum = 0;
-    result.forEach(num =>
-            sum += num
-    )
+    result.forEach(num => sum += num);
     return sum;
 }
-
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
